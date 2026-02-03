724d37e952470803540caca642e07fa1308df7d9
alexey-y-a <lexus2157@mail.ru>
2026-02-04 00:11:39 +0300

    step_003 done

--- libs/logger/logger.go
+++ libs/logger/logger.go
@@ -1,3 +1,30 @@
 package logger
 
-func Init() {}
+import (
+	"io"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+
+var (
+    globalLogger zerolog.Logger
+    once sync.Once
+)
+func Init() {
+    once.Do(func() {
+        var output io.Writer = os.Stdout
+
+        globalLogger = zerolog.New(output).With().Timestamp().Logger()
+
+        zerolog.TimeFieldFormat = time.RFC3339
+    })
+}
+
+func L() zerolog.Logger {
+    Init()
+    return globalLogger
+}
