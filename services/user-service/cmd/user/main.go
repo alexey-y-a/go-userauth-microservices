59bc51dab1bbca610d20e3822c07fdc228bec454
alexey-y-a <lexus2157@mail.ru>
2026-02-05 00:40:59 +0300

    step_004 done

--- services/user-service/cmd/user/main.go
+++ services/user-service/cmd/user/main.go
@@ -1,3 +1,63 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/alexey-y-a/go-userauth-microservices/libs/logger"
+)
+
+func main() {
+    logger.Init()
+
+    log := logger.L().With().Str("service", "user-service").Logger()
+
+    mux := http.NewServeMux()
+
+    mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+        _, err := w.Write([]byte(`{"status":"ok"}`))
+        if err != nil {
+            log.Error().Err(err).Msg("failed to write health response")
+            return
+        }
+    })
+
+    addr := ":8081"
+
+    server := &http.Server {
+        Addr: addr,
+        Handler: mux,
+    }
+
+    sigChan := make(chan os.Signal, 1)
+
+    signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+    go func() {
+        log.Info().Str("addr", addr).Msg("starting user-service")
+        err := server.ListenAndServe()
+        if err != nil &&  err != http.ErrServerClosed {
+            log.Error().Err(err).Msg("user-service stopped with error")
+        }
+    }()
+
+    sig := <- sigChan
+
+    log.Info().Str("signal", sig.String()).Msg("received shutdown signal")
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    err := server.Shutdown(ctx)
+    if err != nil {
+        log.Error().Err(err).Msg("user-service graceful shutdown failed")
+    } else {
+        log.Info().Msg("user-service stopped gracefully")
+    }
+}
